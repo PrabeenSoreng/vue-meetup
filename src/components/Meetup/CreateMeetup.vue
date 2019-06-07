ff814f34bf077e4800fe55131fb580423aae7cd3
Prabeen <prabeensoreng2119@gmail.com>
2019-06-07 12:09:55 +0530

    organize meetup form implemented

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,104 @@
 <template>
-  <div>
-    <h1>Create Meetup Page</h1>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h2 class="display-2 primary--text">Create a new Meetup</h2>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="onCreateMeetup">
+          <v-layout row>
+            <v-flex xs12v sm6 offset-sm3>
+              <v-text-field
+                label="Title"
+                v-model="title"
+                :rules="[v => !!v || 'Title is required.']"
+                name="title"
+                id="title"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12v sm6 offset-sm3>
+              <v-text-field
+                label="Location"
+                v-model="location"
+                :rules="[v => !!v || 'Location is required.']"
+                name="location"
+                id="location"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12v sm6 offset-sm3>
+              <v-text-field
+                label="Image URL"
+                v-model="imageUrl"
+                :rules="[v => !!v || 'Image Url is requird.']"
+                name="imageUrl"
+                id="image-url"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12v sm6 offset-sm3>
+              <img :src="imageUrl" height="150">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12v sm6 offset-sm3>
+              <v-textarea
+                label="Descirption"
+                v-model="description"
+                :rules="[v => !!v || 'Description is required.']"
+                name="description"
+                id="description"
+                required
+              ></v-textarea>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn type="submit" class="primary" :disabled="!valid">Create Meetup</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      valid: true,
+      title: "",
+      location: "",
+      imageUrl: "",
+      description: ""
+    };
+  },
+  methods: {
+    onCreateMeetup() {
+      if (this.$refs.form.validate()) {
+        this.valid = true;
+        const meetupData = {
+          title: this.title,
+          location: this.location,
+          imageUrl: this.imageUrl,
+          description: this.description,
+          date: new Date()
+        };
+        this.$store.dispatch("createMeetup", meetupData);
+        this.$refs.form.reset();
+        this.$router.push("/meetups");
+      }
+    }
+  }
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -33,8 +33,25 @@ export default new Vuex.Store({
       registeredMeetups: ["1"]
     }
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    createMeetup(state, payload) {
+      state.loadedMeetups.push(payload);
+    }
+  },
+  actions: {
+    createMeetup({ commit }, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: "xyz"
+      };
+      // Reach out to firebase and store it.
+      commit("createMeetup", meetup);
+    }
+  },
   getters: {
     loadedMeetups(state) {
       return state.loadedMeetups.sort(
